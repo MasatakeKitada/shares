df2bf2bf9c5b082e8771aa3f70432b7eb18b46c6
MasatakeKitada <mkitada0319@gmail.com>
2015-11-29 15:57:16 +0900

    保存された記事を最新のものから順に表示する機能実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < LayoutController
   def index
-    @pages = Page.where(group_id: params[:group_id]).page(params[:page]).per(6)
+    @pages = Page.where(group_id: params[:group_id]).page(params[:page]).per(6).order("created_at DESC")
     @group = Group.find(params[:group_id])
     @page = Page.new
   end
