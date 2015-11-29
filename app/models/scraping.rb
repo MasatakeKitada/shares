d65c51f80f850a5b632ade080921f1e55e484d50
MasatakeKitada <mkitada0319@gmail.com>
2015-11-29 15:03:30 +0900

    scrapingエラー処理を追加

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,22 +2,16 @@ class Scraping
   def self.page_titles(page_params)
     agent = Mechanize.new
     p = agent.get(page_params[:url])
-
     title = p.at('title').inner_text
-    got_tag = p.at('meta[property="og:image"]')
-    got_url = got_tag[:content]
-    # if meta_tag = p.at('meta[property="og:image"]')
-    #   got_url = meta_tag[:content]
-    # elsif meta_tag = p.at...
-    #   got_url = meta_tag[:content]
-    # elsif meta_tag = p.at...
-    #   got_url = meta_tag[:content]
-    # elsif meta_tag = p.at...
-    #   got_url = meta_tag[:content]
-    # elsif meta_tag = p.at...
-    #   got_url = meta_tag[:content]
-    # else
-    # end
+
+    if got_tag = p.at('meta[property="og:image"]')
+       got_url = got_tag[:content]
+    elsif meta_tag = p.at('meta[property="og:image"]').blank?
+       got_url = p.at(:img).get_attribute('src')
+    else
+        got_url = '../images/no_image.png'
+    end
+    
     group = Group.find page_params[:group_id]
     # group.pages.where(title: title).first_or_initialize(image_url: got_url, url: page_params[:url])
     page = group.pages.where(title: title).first_or_initialize(image_url: got_url)
